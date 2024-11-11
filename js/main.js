e6f05e253b0c4647408ab482401461de20969396
miroljubanastasov <miroljub.a@gmail.com>
2024-11-11 16:39:03 +0100

    Website Redesign

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var parallax = document.querySelector(".parallax");
+
+window.addEventListener('scroll', function() {
+    var scrolled = window.scrollY;
+    var coords = '50% ' + -(scrolled * 0.05) + 'px';
+    parallax.style.backgroundPosition = coords;
+  });
