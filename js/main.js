af7ddfb3d507feb4a3d27811a69a612b15a0f23e
miroljubanastasov <miroljub.a@gmail.com>
2024-11-11 21:37:57 +0100

    again

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var parallax = document.querySelector(".parallax");
 
-/*window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {
     var scrolled = window.scrollY;
     var coords = '50% ' + -(scrolled * 0.05) + 'px';
     parallax.style.backgroundPosition = coords;
-  });*/
+  });
