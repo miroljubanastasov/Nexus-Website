52a56d0238d4c551e3b752512cd07a6b9a45399a
miroljubanastasov <miroljub.a@gmail.com>
2024-11-12 08:53:08 +0100

    Paarllax 0.15

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var parallax = document.querySelector(".parallax");
 
 window.addEventListener('scroll', function() {
     var scrolled = window.scrollY;
-    var coords = '50% ' + -(scrolled * 0.05) + 'px';
+    var coords = '50% ' + -(scrolled * 0.15) + 'px';
     parallax.style.backgroundPosition = coords;
   });
