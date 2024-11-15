00cff2a421a53b3ba0bed64acc30dfefb52a11a4
miroljubanastasov <miroljub.a@gmail.com>
2024-11-15 16:16:27 +0100

    Fixed scrolling issue

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
-// Window resizing issue, set the property --vh
-function setProp(){
-  let vh = window.innerHeight*0.01;
-  document.documentElement.style.setProperty(`--vh`, `${vh}px`);
-}
+// // Window resizing issue, set the property --vh
+// function setProp(){
+//   let vh = window.innerHeight*0.01;
+//   document.documentElement.style.setProperty(`--vh`, `${vh}px`);
+// }
 
-setProp();
-window.addEventListener('resize', setProp);
+// setProp();
+// window.addEventListener('resize', setProp);
 
 // Parallax effect
 var parallax = document.querySelector(".parallax");
 
 window.addEventListener('scroll', function() {
     var scrolled = window.scrollY;
-    var coords = '50% ' + -(scrolled * 0.15) + 'px';
+    var coords = '50% ' + (scrolled * 0.7) + 'px';
     parallax.style.backgroundPosition = coords;
   });
