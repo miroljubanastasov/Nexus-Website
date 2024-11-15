901f7c5bb5450ae4d5e2ea268e79d6d16ed662be
miroljubanastasov <miroljub.a@gmail.com>
2024-11-15 11:33:13 +0100

    js window sizing solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+
+// Window resizing issue, set the property --vh
+function setProp(){
+  let vh = window.innerHeight*0.01;
+  document.documentElement.style.setProperty(`--vh`, `${vh}px`);
+}
+
+setProp();
+window.addEventListener('resize', setProp);
+
+// Parallax effect
 var parallax = document.querySelector(".parallax");
 
 window.addEventListener('scroll', function() {
